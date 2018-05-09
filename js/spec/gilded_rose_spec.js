6fd7db78a61e96b686c24bff5316df2533a6a2c3
Koes Bong <koesbong@gmail.com>
2018-05-09 12:47:45 -0700

    refactored item update and expectations

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,90 +1,59 @@
 describe('Gilded Rose', function() {
+  let item;
+
+  function runTest({sellInExpectation, qualityExpectation}) {
+    item.update();
+    expect(item.sell_in).toEqual(sellInExpectation);
+    expect(item.quality).toEqual(qualityExpectation);
+  }
 
   it('should update Elixir of the Mongoose', function() {
-    let item = new BaseItem('Elixir of the Mongoose', 2, 2);
-    item.update();
-    expect(item.sell_in).toEqual(1);
-    expect(item.quality).toEqual(1);
-
-    item.update();
-    expect(item.sell_in).toEqual(0);
-    expect(item.quality).toEqual(0);
-
-    item.update();
-    expect(item.sell_in).toEqual(-1);
-    expect(item.quality).toEqual(0);
+    item = new BaseItem('Elixir of the Mongoose', 2, 2);
+    runTest({sellInExpectation: 1, qualityExpectation: 1});
+    runTest({sellInExpectation: 0, qualityExpectation: 0});
+    runTest({sellInExpectation: -1, qualityExpectation: 0});
   });
 
   it('should update Conjured Mana Cake', function() {
-    let item = new ConjuredManaCake('Conjured Mana Cake', 3, 6);
-    item.update();
-    expect(item.sell_in).toEqual(2);
-    expect(item.quality).toEqual(4);
-
-    item.update();
-    expect(item.sell_in).toEqual(1);
-    expect(item.quality).toEqual(2);
+    item = new ConjuredManaCake('Conjured Mana Cake', 3, 6);
+    runTest({sellInExpectation: 2, qualityExpectation: 4});
+    runTest({sellInExpectation: 1, qualityExpectation: 2});
 
     item = new ConjuredManaCake('Conjured Mana Cake', 1, 1);
-    item.update();
-    expect(item.sell_in).toEqual(0);
-    expect(item.quality).toEqual(0);
-
-    item.update();
-    expect(item.sell_in).toEqual(-1);
-    expect(item.quality).toEqual(0);
+    runTest({sellInExpectation: 0, qualityExpectation: 0});
+    runTest({sellInExpectation: -1, qualityExpectation: 0});
   });
 
   it('should update Sulfuras', function() {
-    let item = new Sulfuras('Sulfuras, Hand of Ragnaros', 0, 80);
-    item.update();
-    expect(item.sell_in).toEqual(0);
-    expect(item.quality).toEqual(80);
+    item = new Sulfuras('Sulfuras, Hand of Ragnaros', 0, 80);
+    runTest({sellInExpectation: 0, qualityExpectation: 80});
 
     item = new Sulfuras('Sulfuras, Hand of Ragnaros', -1, 80);
-    item.update();
-    expect(item.sell_in).toEqual(-1);
-    expect(item.quality).toEqual(80);
+    runTest({sellInExpectation: -1, qualityExpectation: 80});
   });
 
   it('should update Aged Brie', function() {
-    let item = new AgedBrie('Aged Brie', 2, 0);
-    item.update();
-    expect(item.sell_in).toEqual(1);
-    expect(item.quality).toEqual(1);
-
-    item.update();
-    expect(item.sell_in).toEqual(0);
-    expect(item.quality).toEqual(2);
-
-    item.update();
-    expect(item.sell_in).toEqual(-1);
-    expect(item.quality).toEqual(4);
+    item = new AgedBrie('Aged Brie', 2, 0);
+    runTest({sellInExpectation: 1, qualityExpectation: 1});
+    runTest({sellInExpectation: 0, qualityExpectation: 2});
+    runTest({sellInExpectation: -1, qualityExpectation: 4});
   });
 
   it('should update Backstage Passes', function() {
     // > 10
-    let item = new BackstagePasses('Backstage passes to a concert', 15, 20);
-    item.update();
-    expect(item.sell_in).toEqual(14);
-    expect(item.quality).toEqual(21);
+    item = new BackstagePasses('Backstage passes to a concert', 15, 20);
+    runTest({sellInExpectation: 14, qualityExpectation: 21});
 
     // > 5, < 10
     item = new BackstagePasses('Backstage passes to a concert', 8, 20);
-    item.update();
-    expect(item.sell_in).toEqual(7);
-    expect(item.quality).toEqual(22);
+    runTest({sellInExpectation: 7, qualityExpectation: 22});
 
     // > 0, < 5
     item = new BackstagePasses('Backstage passes to a concert', 1, 20);
-    item.update();
-    expect(item.sell_in).toEqual(0);
-    expect(item.quality).toEqual(23);
+    runTest({sellInExpectation: 0, qualityExpectation: 23});
 
     // <= 0
     item = new BackstagePasses('Backstage passes to a concert', 0, 20);
-    item.update();
-    expect(item.sell_in).toEqual(-1);
-    expect(item.quality).toEqual(0);
+    runTest({sellInExpectation: -1, qualityExpectation: 0});
   });
 });
