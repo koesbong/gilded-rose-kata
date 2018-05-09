e379af420c78a075b87599449be76c905f6345b2
Koes Bong <koesbong@gmail.com>
2018-05-08 23:22:38 -0700

    refactored code

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,9 +1,27 @@
-describe("Gilded Rose", function() {
+describe('Gilded Rose', function() {
 
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
-    update_quality();
-    expect(items[0].name).toEqual("fixme");
+  it('should update Conjured Mana Cake', function() {
+    let item = new ConjuredManaCake('Conjured Mana Cake', 3, 6);
+    item.update();
+    expect(item.sell_in).toEqual(2);
+    expect(item.quality).toEqual(4);
+
+    item.update();
+    expect(item.sell_in).toEqual(1);
+    expect(item.quality).toEqual(2);
   });
 
+  it('should update Sulfuras', function() {
+    let item = new Sulfuras('Sulfuras, Hand of Ragnaros', 0, 80);
+    item.update();
+    expect(item.sell_in).toEqual(0);
+    expect(item.quality).toEqual(80);
+
+    item = new Sulfuras('Sulfuras, Hand of Ragnaros', -1, 80);
+    item.update();
+    expect(item.sell_in).toEqual(-1);
+    expect(item.quality).toEqual(80);
+  });
+
+  // TODO: Add more tests for the rest of the items
 });
--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -1,55 +1,90 @@
+// Do not alter the item function as that belongs to the goblin in the
+// corner who will insta-rage and one-shot you as he doesn't believe
+// in shared code ownership.
 function Item(name, sell_in, quality) {
   this.name = name;
   this.sell_in = sell_in;
   this.quality = quality;
 }
 
-var items = []
+class BaseItem extends Item {
+  update() {
+    this.updateQuality();
+    this.updateSellIn();
+  }
 
-function update_quality() {
-  for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
-        }
-      }
+  updateQuality() {
+    let quality;
+
+    if (this.sell_in > 0) {
+      quality = this.quality - 1;
     } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-        }
-      }
+      quality = this.quality - 2;
     }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
+
+    if (quality >= 0) {
+      this.quality = quality;
     }
-    if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
-            }
-          }
-        } else {
-          items[i].quality = items[i].quality - items[i].quality
-        }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
-        }
-      }
+  }
+
+  updateSellIn() {
+    this.sell_in = this.sell_in - 1;
+  }
+}
+
+class Sulfuras extends BaseItem {
+	updateQuality() {}
+  updateSellIn() {}
+}
+
+class ConjuredManaCake extends BaseItem {
+  updateQuality() {
+    let quality = this.quality - 2;
+
+    if (quality >= 0) {
+      this.quality = quality;
     }
+  }
+}
+
+class AgedBrie extends BaseItem {
+  updateQuality() {
+    let quality;
+
+    if (this.sell_in > 0) {
+      quality = this.quality + 1;
+    } else {
+      quality = this.quality + 2;
+    }
+
+    if (quality <= 50) {
+      this.quality = quality;
+    }
+  }
+}
+
+class BackstagePasses extends BaseItem {
+  updateQuality() {
+    let quality = 0;
+
+    if (this.sell_in > 10) {
+      quality = this.quality + 1;
+    } else if (this.sell_in > 5) {
+      quality = this.quality + 2;
+    } else if (this.sell_in > 0) {
+      quality = this.quality + 3;
+    }
+
+    this.quality = quality > 50 ? 50 : quality;
+  }
+}
+
+class GildedRoseStore {
+  constructor(items = []) {
+    this.items = items;
+  }
+
+  updateItem() {
+    this.items.map(item => item.update());
   }
 }
